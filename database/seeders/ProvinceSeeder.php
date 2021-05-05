f9c0539c0ae7644a14db6de9827d3d0631008fb4
ahmadkarimi2009@gmail.com <ahmadkarimi2009@gmail.com>
2021-05-05 02:36:21 -0700

    bootrap added, home page changed, province added

---
+++ database/seeders/ProvinceSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Province;
+
+class ProvinceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $provinces = [
+            ['name' =>'Kabul'],
+            ['name' =>'Laghman'],
+            ['name' =>'Jawzjan'],
+            ['name' =>'Mazar'],
+            ['name' =>'Kandahar'],
+            ['name' =>'Nangarhar'],
+            ['name' =>'Badakhshan'],
+            ['name' =>'Bamyan'],
+            ['name' =>'Herat'],
+            ['name' =>'Kapisa']
+        ];
+
+        Province::insert($provinces);
+    }
+}

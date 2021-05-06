481fb121014e28725f8a38adef01eb77e3dbe258
ahmadkarimi2009@gmail.com <ahmadkarimi2009@gmail.com>
2021-05-05 22:33:47 -0700

    relationships added for the modules

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    public function province() {
+        return $this->belongsTo(province::class, 'province');
+    }
+
+    public function topic() {
+        return $this->belongsTo(Topic::class, 'topic');
+    }
 }

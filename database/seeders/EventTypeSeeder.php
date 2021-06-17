1dd36e6f7669bfe6bddda11ad2326dd5c3d353d9
ahmadkarimi2009@gmail.com <ahmadkarimi2009@gmail.com>
2021-06-17 09:41:21 +0430

    images field added to every activity type

--- database/seeders/EventTypeSeeder.php
+++ database/seeders/EventTypeSeeder.php
@@ -17,63 +17,63 @@ class EventTypeSeeder extends Seeder
         $fields = [
             [
                 'name' =>'Contact with partner/authority (meeting, workshop, presentation…)',
-                'fields' => '[2,21]',
+                'fields' => '[2,21,27]',
             ],
             [
                 'name' =>'Moderated Socio-Cultural Dialogues',
-                'fields' => '[1,9,10,11,12,26,19]',
+                'fields' => '[1,9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Online Cultural Dialogues',
-                'fields' => '[15,9,14,10,11,12,26,19]',
+                'fields' => '[15,9,14,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Thematic Events',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Online Events',
-                'fields' => '[15,9,14,10,11,12,19]',
+                'fields' => '[15,9,14,10,11,12,19,27]',
             ],
             [
                 'name' =>'New Voluteers Joined',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Meeting With Volunteers',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'New Care Givers',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Care Givers Training',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Support group held by Care Giver',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Number of teachers recruited for training',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Teacher trainings',
-                'fields' => '[9,10,11,12,26,19]',
+                'fields' => '[9,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Support groups (PSC)',
-                'fields' => '[3,10,11,12,26,19]',
+                'fields' => '[3,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Life Skills Group (PSCW)',
-                'fields' => '[3,10,11,12,26,19]',
+                'fields' => '[3,10,11,12,26,19,27]',
             ],
             [
                 'name' =>'Online Counseling Sessions',
-                'fields' => '[14,10,11,12,26,19]',
+                'fields' => '[14,10,11,12,26,19,27]',
             ]
         ];
         EventType::insert($fields);
--- database/seeders/FieldsSeeder.php
+++ database/seeders/FieldsSeeder.php
@@ -20,175 +20,175 @@ class FieldsSeeder extends Seeder
                 'table_name' => '',
                 'machine_name' => 'in_scc_or_field',
                 'data_type' => 'checkbox',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' => 'Partner/Authority',
                 'table_name' => '',
                 'machine_name' => 'partner_authority',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' => 'Group Topic',
                 'table_name' => '',
                 'machine_name' => 'group_topic',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' => 'Male Beneficiaries',
                 'table_name' => '',
                 'machine_name' => 'male_beneficiaries',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' => 'Female Beneficiaries',
                 'table_name' => '',
                 'machine_name' => 'female_eneficiaries',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' => 'Total Beneficiaries',
                 'table_name' => '',
                 'machine_name' => 'total_eneficiaries',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' => 'Project of male or female Young Leaders?',
                 'table_name' => '',
                 'machine_name' => 'project_of_male_or_female_young_leaders',
                 'data_type' => 'text',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' => 'Project of male or female volunteers?',
                 'table_name' => '',
                 'machine_name' => 'project_of_male_or_female_volunteers',
                 'data_type' => 'text',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' => 'Topic',
                 'table_name' => '',
                 'machine_name' => 'topic',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'Number of Male',
                 'table_name' => '',
                 'machine_name' => 'number_of_male',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Number of Female',
                 'table_name' => '',
                 'machine_name' => 'number_of_female',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Total',
                 'table_name' => '',
                 'machine_name' => 'total',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Number of Groups',
                 'table_name' => '',
                 'machine_name' => 'number_of_groups',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Number of Sessions',
                 'table_name' => '',
                 'machine_name' => 'number_of_sessions',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'With Province',
                 'table_name' => 'provinces',
                 'machine_name' => 'with_province',
                 'data_type' => 'selectbox',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'Location',
                 'table_name' => '',
                 'machine_name' => 'location',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'Date of Trip',
                 'table_name' => '',
                 'machine_name' => 'date_of_trip',
                 'data_type' => 'date',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Supervisor',
                 'table_name' => '',
                 'machine_name' => 'supervisor',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'Details',
                 'table_name' => '',
                 'machine_name' => 'details',
                 'data_type' => 'textarea',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Stakeholder',
                 'table_name' => '',
                 'machine_name' => 'stakeholder',
                 'data_type' => 'text',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'Activity Details',
                 'table_name' => '',
                 'machine_name' => 'activity_details',
                 'data_type' => 'textarea',
-                'searchable' => 'true'
+                'searchable' => 'true',
             ],
             [
                 'name' =>'No. of online counseling sessions arranged in container',
                 'table_name' => '',
                 'machine_name' => 'no_of_online_counseling_sessions_arranged_in_container',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'No. of online counseling sessions',
                 'table_name' => '',
                 'machine_name' => 'no_of_online_counseling_sessions',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'No. of sessions for male clients',
                 'table_name' => '',
                 'machine_name' => 'no_of_sessions_for_male_clients',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'No. of sessions for female clients',
                 'table_name' => '',
                 'machine_name' => 'no_of_sessions_for_male_clients',
                 'data_type' => 'number',
-                'searchable' => ''
+                'searchable' => 'false',
             ],
             [
                 'name' =>'Date',
@@ -196,6 +196,13 @@ class FieldsSeeder extends Seeder
                 'machine_name' => 'date_of_activity',
                 'data_type' => 'date',
                 'searchable' => 'true'
+            ],
+            [
+                'name' =>'Images',
+                'table_name' => '',
+                'machine_name' => 'images',
+                'data_type' => 'file',
+                'searchable' => 'false'
             ]
         ];
 
